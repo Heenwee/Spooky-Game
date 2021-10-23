a4979ef4891da8d70f2695f99895431dfe31a71d
Henwee <heenwee@gmail.com>
2021-10-23 23:54:17 +0200

    Branches now stop at intersection

--- Spooky Game/Assets/Scripts/MapGeneration.cs
+++ Spooky Game/Assets/Scripts/MapGeneration.cs
@@ -7,13 +7,17 @@ public class MapGeneration : MonoBehaviour
     public int loops;
     public float sideSectionLength, upSectionLength;
     public int branchLenghtMin, branchLenghtMax;
+    public int branchNr = 5;
     Transform[] points;
+    List<Vector2> pointPos;
     int lastDir; // 1 = left, 2 = right, 3 = up
 
     // Start is called before the first frame update
     void Start()
     {
         GameObject pointObj = new GameObject("point");
+        pointPos = new List<Vector2>();
+
         points = new Transform[loops];
         for(int i = 0; i < loops; i++)
         {
@@ -33,50 +37,61 @@ public class MapGeneration : MonoBehaviour
     }
     void Generate(Transform[] p, int lenght)
     {
+        bool stop = false;
+        int generatedPoints = 0;
         for (int i = 1; i < lenght; i++)
         {
-            int dir = Random.Range(1, 4);
-
-            if ((dir == 1 && lastDir != 2) ||
-                (dir == 2 && lastDir != 1) ||
-                dir == 3)
+            if(!stop)
             {
-                Vector3 change = Vector3.zero;
-                if (dir == 1)
-                {
-                    change = Vector2.left * sideSectionLength;
-                }
-                if (dir == 2)
-                {
-                    change = Vector2.right * sideSectionLength;
-                }
-                if (dir == 3)
-                {
-                    change = Vector2.up * upSectionLength;
-                }
-                p[i].position = p[i - 1].position + change;
+                int dir = Random.Range(1, 4);
 
-                lastDir = dir;
+                if ((dir == 1 && lastDir != 2) ||
+                    (dir == 2 && lastDir != 1) ||
+                    dir == 3)
+                {
+                    Vector3 change = Vector3.zero;
+                    if (dir == 1)
+                    {
+                        change = Vector2.left * sideSectionLength;
+                    }
+                    if (dir == 2)
+                    {
+                        change = Vector2.right * sideSectionLength;
+                    }
+                    if (dir == 3)
+                    {
+                        change = Vector2.up * upSectionLength;
+                    }
+                    p[i].position = p[i - 1].position + change;
+
+                    lastDir = dir;
+
+                    foreach (Vector3 pos in pointPos)
+                    {
+                        if (p[i].position == pos) stop = true;
+                    }
+
+                    generatedPoints++;
+                    pointPos.Add(p[i].position);
+                }
+                else i--;
             }
-            else i--;
         }
-        LineRenderer mainLine = new GameObject().AddComponent<LineRenderer>();
-        mainLine.positionCount = lenght;
-        for (int j = 0; j < mainLine.positionCount; j++)
+        LineRenderer line = new GameObject().AddComponent<LineRenderer>();
+        line.positionCount = generatedPoints;
+        for (int j = 0; j < line.positionCount; j++)
         {
-            mainLine.SetPosition(j, p[j].position);
+            line.SetPosition(j, p[j].position);
         }
     }
     IEnumerator GenerateMain()
     {
         Generate(points, loops);
         yield return new WaitForSeconds(1);
-        GenerateBranches();
+        for (int i = 0; i < branchNr; i++) GenerateBranches();
     }
     void GenerateBranches()
     {
-        for (int j = 0; j < 5; j++)
-        {
             int branchLenght = Random.Range(branchLenghtMin, branchLenghtMax + 1);
 
             GameObject pointObj = new GameObject("branchPoint");
@@ -89,6 +104,5 @@ public class MapGeneration : MonoBehaviour
             bPoints[0] = points[Random.Range(0, points.Length)];
 
             Generate(bPoints, branchLenght);
-        }
     }
 }
